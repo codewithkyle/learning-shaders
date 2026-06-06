2556f1023836293cb80609f2f5c3fa98f3b497c2
Kyle Andrews <codingwithkyle@gmail.com>
2026-06-06 13:44:57 -0400

    Smoothstep example

--- lesson-3/shaders/frag.glsl
+++ lesson-3/shaders/frag.glsl
@@ -9,8 +9,9 @@ void main() {
     vec3 red = vec3(1.0, 0.0, 0.0);
     //color = vec3(vUvs.x);
     //color = vec3(step(0.25, vUvs.x));
-
-    color = mix(red, color, vUvs.x);
+    //color = mix(red, color, vUvs.x);
+    //color = vec3(smoothstep(0.0, 1.0, vUvs.x));
+    color = mix(red, color, smoothstep(0.0, 1.0, vUvs.x));
 
     finalColor = vec4(color, 1.0);
 }
