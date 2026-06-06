938751baa4a669e059cd0ca96ea854193996aad8
Kyle Andrews <codingwithkyle@gmail.com>
2026-06-06 14:04:28 -0400

    Comparing mix vs smoothstep

--- lesson-3/shaders/frag.glsl
+++ lesson-3/shaders/frag.glsl
@@ -7,11 +7,20 @@ in vec2 vUvs;
 void main() {
     vec3 color = vec3(0.0);
     vec3 red = vec3(1.0, 0.0, 0.0);
-    //color = vec3(vUvs.x);
-    //color = vec3(step(0.25, vUvs.x));
-    //color = mix(red, color, vUvs.x);
-    //color = vec3(smoothstep(0.0, 1.0, vUvs.x));
-    color = mix(red, color, smoothstep(0.0, 1.0, vUvs.x));
+    vec3 blue = vec3(0.0, 0.0, 1.0);
+    vec3 white = vec3(1.0);
+
+    float line = smoothstep(0.0, 0.005, abs(vUvs.y - 0.5));
+
+    if (vUvs.y > 0.5) {
+        color = mix(red, blue, vUvs.x);
+    }
+    else
+    {
+        color = mix(red, blue, smoothstep(0.0, 1.0, vUvs.x));
+    }
+
+    color = mix(white, color, line);
 
     finalColor = vec4(color, 1.0);
 }
