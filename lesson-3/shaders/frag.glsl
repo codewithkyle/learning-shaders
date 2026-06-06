cb599ba669b081092ebc1221d3394db93711a70f
Kyle Andrews <codingwithkyle@gmail.com>
2026-06-06 13:41:25 -0400

    Mix example

--- lesson-3/shaders/frag.glsl
+++ lesson-3/shaders/frag.glsl
@@ -6,7 +6,11 @@ in vec2 vUvs;
 
 void main() {
     vec3 color = vec3(0.0);
+    vec3 red = vec3(1.0, 0.0, 0.0);
     //color = vec3(vUvs.x);
-    color = vec3(step(0.25, vUvs.x));
+    //color = vec3(step(0.25, vUvs.x));
+
+    color = mix(red, color, vUvs.x);
+
     finalColor = vec4(color, 1.0);
 }
